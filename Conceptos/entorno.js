33556d682caf95a72d28aca6500adf68471fd4ae
serbeld <serbeldiaz@gmail.com>
2021-10-23 10:14:53 -0500

    Testing nodemon

--- Conceptos/entorno.js
+++ Conceptos/entorno.js
@@ -4,3 +4,6 @@ let web = process.env.WEB || 'https://github.com/Serbeld';
 
 console.log('Hello ' + nombre);
 console.log('My web page is ' + web);
+
+// sudo npm install -g nodemon
+console.log("This line is created and viewed By nodemon");

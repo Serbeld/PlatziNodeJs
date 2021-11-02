9163150aadf23c7048a275bfe9845a8de427dda4
serbeld <serbeldiaz@gmail.com>
2021-11-02 00:07:49 -0500

    Update promises to catch errors

--- async/promises.js
+++ async/promises.js
@@ -7,9 +7,37 @@ function hello(user) {
     });
 }
 
+function talk(user) {
+    return new Promise((resolve, reject) => {
+        setTimeout(function () {
+            console.log("Blah Blah Blah Blah...");
+            // resolve(user);
+            reject('Error in talk function');
+        }, 1000);
+    });
+}
+
+function bye(user) {
+    return new Promise((resolve, reject) => {
+        setTimeout(function () {
+            console.log("Bye", user);
+            resolve();
+        }, 2000);
+    });
+}
+
 // Promises 
 console.log("Starting Process...");
+
 hello('Sergio')
+    .then(talk)
+    .then(talk)
+    .then(talk)
+    .then(bye)
     .then((user) => {
         console.log('Ending Process...');
     })
+    .catch(error => {
+        console.error("ERROR!!");
+        console.error(error);
+    });
